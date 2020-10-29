efbb5040eee0cb9a55a2ff08e82b2403b63ae3f3
Brijesh Panchal <brijeshpanchal127@gmail.com>
2020-10-28 22:23:41 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [result, setResult] = useState("");
+
+
+  function clickHandle(e) {
+    setResult(result + e.target.name);
+
+  }
+
+  function backSpace() {
+    setResult(result.slice(0, result.length - 1));
+  }
+
+  function clear() {
+    setResult("");
+  }
+
+  function calculation() {
+    try {
+      setResult(eval(result) + "");
+
+    }
+    catch (error) {
+      setResult("error");
+    }
+  }
+
+  return (
+    <div className="calculator">
+      <form>
+        <input type="text" value={result} />
+
+      </form>
+      <div className="keypad">
+        <button name="(" onClick={clickHandle}>(</button>
+        <button onClick={clear}>CE</button>
+        <button name=")" onClick={clickHandle}>)</button>
+        <button onClick={backSpace}>C</button>
+        <button name="1" onClick={clickHandle}>1</button>
+        <button name="2" onClick={clickHandle}>2</button>
+        <button name="3" onClick={clickHandle}>3</button>
+        <button name="+" onClick={clickHandle}>+</button>
+        <button name="4" onClick={clickHandle}>4</button>
+        <button name="5" onClick={clickHandle}>5</button>
+        <button name="6" onClick={clickHandle}>6</button>
+        <button name="-" onClick={clickHandle}>-</button>
+        <button name="7" onClick={clickHandle}>7</button>
+        <button name="8" onClick={clickHandle}>8</button>
+        <button name="9" onClick={clickHandle}>9</button>
+        <button name="*" onClick={clickHandle}>*</button>
+        <button name="." onClick={clickHandle}>.</button>
+        <button name="0" onClick={clickHandle}>0</button>
+        <button onClick={calculation}>=</button>
+        <button name="/" onClick={clickHandle}>/</button>
+
+      </div>
+
+    </div>
+  )
+
+
+}
+
+
+
+
+
+
+
+export default App;
